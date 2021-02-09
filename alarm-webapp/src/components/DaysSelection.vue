4430fe1e1fb1399a2ccc061d8838013fdc0b299f
Peter Uithoven <peter@peteruithoven.nl>
2021-02-10 00:44:45 +0100

    Switch to weekdays

--- alarm-webapp/src/components/DaysSelection.vue
+++ alarm-webapp/src/components/DaysSelection.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex">
     <button
-      v-for="day of days"
+      v-for="(day, weekday) in days"
       :key="day"
       class="uppercase py-1 px-2 border-gray-200 border border-r-0 first:rounded-l-md last:rounded-r-md last:border-r"
-      :class="{ 'bg-enabled text-gray-100': modelValue.includes(day) }"
-      @click="toggle(day)"
+      :class="{ 'bg-enabled text-gray-100': modelValue.includes(weekday) }"
+      @click="toggle(weekday)"
     >
       {{ day }}
     </button>
@@ -13,17 +13,19 @@
 </template>
 
 <script setup>
+// modelValue contains list of enabled weekdays.
+// weekday is an integer, where Monday is 0 and Sunday is 6
 const props = defineProps({ modelValue: Array });
 const emit = defineEmit(["update:modelValue"]);
 const days = ["mo", "tu", "we", "th", "fr", "sa", "su"];
 
-function toggle(day) {
+function toggle(weekday) {
   let { modelValue } = props;
   emit(
     "update:modelValue",
-    modelValue.includes(day)
-      ? modelValue.filter((i) => i !== day)
-      : [...modelValue, day]
+    modelValue.includes(weekday)
+      ? modelValue.filter((i) => i !== weekday)
+      : [...modelValue, weekday]
   );
 }
 </script>
