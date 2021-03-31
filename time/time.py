d5c3dfe87434cafa425afda12ac5f657406ab1fe
Peter Uithoven <peter@peteruithoven.nl>
2021-03-31 14:06:55 +0200

    Tweak time brightness

--- time/time.py
+++ time/time.py
@@ -8,7 +8,7 @@ display = Seg7x4(i2c, address=0x70)
 now = datetime.now() # current date and time
 
 daytime = now.hour > 7 and now.hour < 19
-display.brightness = 0.1 if daytime else 0
+display.brightness = 0.4 if daytime else 0
 
 time = now.strftime("%H:%M")
 
