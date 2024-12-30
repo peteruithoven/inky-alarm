0c3ab02b6d2924b9c10ee78574247acecc222ede
peteruithoven <peter@metabolic.nl>
2024-12-30 18:27:02 +0100

    Modernize time script

--- time/time.py
+++ time/time.py
@@ -1,14 +1,16 @@
-import board
+import time
 from datetime import datetime
+import board
+import busio
 from adafruit_ht16k33.segments import Seg7x4
 
-i2c = board.I2C()
+i2c = busio.I2C(board.SCL, board.SDA)
 display = Seg7x4(i2c, address=0x70)
 
 now = datetime.now() # current date and time
 
-daytime = now.hour > 7 and now.hour < 19
-display.brightness = 0.4 if daytime else 0
+daytime = 7 < now.hour < 19
+display.brightness = 0.4 if daytime else 0.0
 
 time = now.strftime("%H:%M")
 
