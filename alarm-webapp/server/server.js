61a448879c645bff0bcf68f36a95cbbd45f4c208
peteruithoven <peter@metabolic.nl>
2024-12-30 14:00:54 +0100

    Write alarms file if it doesn't exist yet and modernize server

--- alarm-webapp/server/server.js
+++ alarm-webapp/server/server.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const { readJSON, writeJSON } = require("@bevry/jsonfile");
-const os = require("os");
-const path = require("path");
+import express from "express";
+import { readJSON, writeJSON } from "@bevry/jsonfile";
+import os from "os";
+import path from "path";
 const app = express();
 const filePath = path.join(os.homedir(), "/alarms.json");
 const port = 3000;
+
+console.log("filePath: ", filePath);
 
 app.use(express.static("dist"));
 app.use(express.json());
@@ -21,18 +23,22 @@ let alarms = [
   }
 ];
 
-readJSON(filePath).then((contents) => {
-  alarms = contents;
-  console.log("alarms: ", alarms);
-});
+try {
+  alarms = await readJSON(filePath);
+} catch (e) {
+  console.log("error reading alarms: ", e);
+  console.log("create new alarms file");
+  await writeJSON(filePath, alarms);
+}
+console.log("alarms: ", alarms);
 
-app.get("/api/alarms", (req, res) => {
+app.get("/api/alarms", (_, res) => {
   res.send(alarms);
 });
 app.post("/api/alarms", (req, res) => {
   // console.log(req.body); // your json
   alarms = req.body;
-  writeJSON(filePath, alarms).then(() => console.log("done"));
+  writeJSON(filePath, alarms).then(() => console.log("updated alarms"));
   res.send(alarms);
 });
 
